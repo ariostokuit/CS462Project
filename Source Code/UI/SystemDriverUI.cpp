4179f8115e350ffaa42c6f2b981ae4dbf1369b39
jamesvu725 <james.vu725@csu.fullerton.edu>
2020-12-06 01:33:38 -0800

    add responds to view product

--- Source Code/UI/SystemDriverUI.cpp
+++ Source Code/UI/SystemDriverUI.cpp
@@ -32,14 +32,14 @@ namespace UI
     {
       _logger << "Starting scenario 1: Purchase Products";
 
-      auto session = Domain::Session::SessionHandler::createSession( {"James", "Vu", {"Customer"}} );
-      auto cart    = Domain::ShoppingCart::ShoppingCartHandler::createShoppingCart();
-                     cart->viewProducts();
-                     cart->addProduct("Pizza", 1);
-      auto order   = cart->initiateCheckout();
-                     order->setOrderInstruction("123 Street", "Keep warm");
-      auto total   = order->calculateTotal();
-      auto message = order->makePayment(1234, "12/06", 567);
+      auto session     = Domain::Session::SessionHandler::createSession( {"James", "Vu", {"Customer"}} );
+      auto cart        = Domain::ShoppingCart::ShoppingCartHandler::createShoppingCart();
+      auto productList = cart->viewProducts();
+                         cart->addProduct("Pizza", 1);
+      auto order       = cart->initiateCheckout();
+                         order->setOrderInstruction("123 Street", "Keep warm");
+      auto total       = order->calculateTotal();
+      auto message     = order->makePayment(1234, "12/06", 567);
       session->signOff();
       _logger << "Completed scenario 1";
 
